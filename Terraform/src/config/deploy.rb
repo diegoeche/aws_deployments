7cfcc85ddbb2b6245f3b3dffb0fc4151cee8d8d8
Diego <diego@geeny.io>
2018-09-18 18:39:20 +0200

    Missing files

--- Terraform/src/config/deploy.rb
+++ Terraform/src/config/deploy.rb
@@ -1,5 +1,57 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "my_app_name"
-set :repository, "git@github.com:diegoeche/aws_deployments.git"
+set :repo_url, "git@github.com:diegoeche/aws_deployments.git"
+set :branch, "master"
+set :application, "terraform"
+set :ssh_options, { :forward_agent => true }
+set :deploy_to, "/home/ubuntu/"
+
+
+require 'json'
+
+namespace :pm2 do
+  def app_status
+    within current_path do
+      ps = JSON.parse(capture :pm2, :jlist, fetch(:app_command))
+      if ps.empty?
+        return nil
+      else
+        # status: online, errored, stopped
+        return ps[0]["pm2_env"]["status"]
+      end
+    end
+  end
+
+  def restart_app
+    within current_path do
+      execute :pm2, :restart, fetch(:app_command)
+    end
+  end
+
+  def start_app
+    within current_path do
+      execute :pm2, :stop, fetch(:app_command)
+    end
+  end
+
+  desc 'Restart app gracefully'
+  task :restart do
+    on roles(:app) do
+      case app_status
+      when nil
+        info 'App is not registerd'
+        start_app
+      when 'stopped'
+        info 'App is stopped'
+        restart_app
+      when 'errored'
+        info 'App has errored'
+        restart_app
+      when 'online'
+        info 'App is online'
+        restart_app
+      end
+    end
+  end
+end
--- Terraform/src/config/deploy/production.rb
+++ Terraform/src/config/deploy/production.rb
@@ -1,5 +1,2 @@
 # Change the ip to your instance IP.
 server "35.168.112.161", :user => "ubuntu", :roles => %{web app}
-
-set :repository, "git@github.com:diegoeche/aws_deployments.git"
-set :branch, "master"
